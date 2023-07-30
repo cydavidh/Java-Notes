8796d67aaea56820c44ba92f40817dd63d0e32f8
cydavidh <52554154+cydavidh@users.noreply.github.com>
2023-07-30 21:40:22 +0800

    july 30, 2023 update

--- n06_array_list/ArrayList1.java
+++ n06_array_list/ArrayList1.java
@@ -1,6 +1,7 @@
 package n06_array_list;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 
 public class ArrayList1 {
@@ -22,12 +23,15 @@ public class ArrayList1 {
         cars.size(); // 4
         cars.get(0); // Volvo
         cars.set(0, "Toyota"); // Volvo -> Toyota
-        cars.remove(0); // Toyota removed
+        cars.remove(0); // output: "Volvo" .remove() returns the removed object
         cars.clear(); // Clear full list
         cars.contains("BMW"); // true //boolean found = cars.contains("BMW");
 
         Collections.sort(cars); // Sort cars
 
+        cars.toString(); // way to print arraylist
+
+        // another way to print arryalist
         for (String i : cars) {
             System.out.println(i);
         }
@@ -35,6 +39,10 @@ public class ArrayList1 {
             String car = cars.get(i);
             System.out.println(car);
         }
+
+        // initialize arraylist with values
+        ArrayList<String> gfg = new ArrayList<String>(Arrays.asList("Geeks", "for", "Geeks"));
+        ArrayList<Integer> books = new ArrayList<Integer>(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 10));
 
     }
 }
---
+++ n24_objects_is_a_reference_variable/Objects.md
@@ -0,0 +1,4 @@
+Object as a method parameter
+Object as an object variable (synonymous with instance variable)
+Objects that contains a list
+List of objects as an object/instance variable
