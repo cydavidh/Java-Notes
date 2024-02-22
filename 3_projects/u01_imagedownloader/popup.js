0e952670d6f671d8f6343451cfc138d80034e50e
cydavidh <52554154+cydavidh@users.noreply.github.com>
2024-02-22 15:34:48 +0800

    update .gitignore

--- 3_projects/u01_imagedownloader/popup.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('download').addEventListener('click', () => {
-    chrome.runtime.sendMessage({ action: "downloadImages" });
-});
-
-document.getElementById('closeTabs').addEventListener('click', () => {
-    chrome.runtime.sendMessage({ action: "closeDownloadedTabs" });
-});
